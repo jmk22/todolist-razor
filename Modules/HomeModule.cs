43ae03b2d20f3c62a87ab8cefa9828a63aa1bc64
Jill Kuchman <pillanoid@gmail.com>
2015-10-06 15:41:33 -0700

    Add constructor to Task class

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,10 +11,10 @@ namespace ToDoList
                 return View["index.html"];
             };
             Post["/task_added"] = _ => {
-              var NewTask = new Task {
-                Description = Request.Form["new-task"]
-              };
-              // var NewTask = new Task (Request.Form["new-task"]);
+              // var NewTask = new Task {
+              //   Description = Request.Form["new-task"]
+              // };
+              var NewTask = new Task (Request.Form["new-task"]);
               // newTask.save();
               // Task.Save(NewTask.getDescription());
               return View["task_added.cshtml", NewTask];
--- Objects/Task.cs
+++ Objects/Task.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 namespace ToDoList
 {
   public class Task{
-    public string Description {get; set;}
-    // private string _description;
-    //
-    // public Task(string taskDescription)
-    // {
-    //   _description = taskDescription;
-    // }
-    // public string getDescription()
-    // {
-    //   return _description;
-    // }
-    // public void setDescription(string newDescription)
-    // {
-    //   _description = newDescription;
-    // }
+    // public string Description {get; set;}
+    private string _description;
+
+    public Task(string taskDescription)
+    {
+      _description = taskDescription;
+    }
+    public string getDescription()
+    {
+      return _description;
+    }
+    public void setDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
   }
 }
