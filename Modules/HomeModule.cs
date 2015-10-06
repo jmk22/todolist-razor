5ceae4b2286b054e281af131b7b57d52996d74be
Jill Kuchman <pillanoid@gmail.com>
2015-10-06 15:37:42 -0700

    Initial commit, works with razor

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,24 @@
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace ToDoList
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                return View["index.html"];
+            };
+            Post["/task_added"] = _ => {
+              var NewTask = new Task {
+                Description = Request.Form["new-task"]
+              };
+              // var NewTask = new Task (Request.Form["new-task"]);
+              // newTask.save();
+              // Task.Save(NewTask.getDescription());
+              return View["task_added.cshtml", NewTask];
+            };
+        }
+    }
+}
---
+++ Objects/Task.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace ToDoList
+{
+  public class Task{
+    public string Description {get; set;}
+    // private string _description;
+    //
+    // public Task(string taskDescription)
+    // {
+    //   _description = taskDescription;
+    // }
+    // public string getDescription()
+    // {
+    //   return _description;
+    // }
+    // public void setDescription(string newDescription)
+    // {
+    //   _description = newDescription;
+    // }
+  }
+}
