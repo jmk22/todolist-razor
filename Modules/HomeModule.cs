36ce6d23e3c9ba3630f5e5888122f04c4fb3abc7
Jill Kuchman <pillanoid@gmail.com>
2015-10-07 09:08:27 -0700

    Add clear tasks route/page/method

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,17 +11,17 @@ namespace ToDoList
                 return View["index.cshtml"];
             };
             Post["/task_added"] = _ => {
-              // var NewTask = new Task {
-              //   Description = Request.Form["new-task"]
-              // };
               var NewTask = new Task (Request.Form["new-task"]);
               NewTask.Save();
-              // Task.Save(NewTask.getDescription());
               return View["task_added.cshtml", NewTask];
             };
             Get["/view_all_tasks"] = _ => {
               var AllTasks = Task.ListOfTasks;
               return View["view_all_tasks.cshtml", AllTasks];
+            };
+            Post["/tasks_cleared"] = _ => {
+              Task.DeleteAll();
+              return View["tasks_cleared.cshtml"];
             };
         }
     }
